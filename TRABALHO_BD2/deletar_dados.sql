0d253fce8ba5874cb0865eda8b75ece9aa2923fe
yvos77 <yuripb902@gmail.com>
2025-09-23 23:05:20 -0300

    Delete TRABALHO_BD2 directory

--- TRABALHO_BD2/deletar_dados.sql
+++
@@ -1,9 +0,0 @@
--- Deletando os dados das tabelas
-DELETE FROM optd_social.Participacao;
-DELETE FROM optd_social.Oportunidade;
-DELETE FROM optd_social.AreaDeAtuacao;
-DELETE FROM optd_social.Instituicao;
-DELETE FROM optd_social.Voluntario;
-DELETE FROM optd_social.Cadastro;
-DELETE FROM optd_social.Beneficiario;
-
--- TRABALHO_BD2/exibir_resultados.sql
+++
@@ -1,8 +0,0 @@
--- Selecionando todas as tabelas
-SELECT * FROM optd_social.Beneficiario;
-SELECT * FROM optd_social.Cadastro;
-SELECT * FROM optd_social.Voluntario;
-SELECT * FROM optd_social.Instituicao;
-SELECT * FROM optd_social.AreaDeAtuacao;
-SELECT * FROM optd_social.Oportunidade;
-SELECT * FROM optd_social.Participacao;
