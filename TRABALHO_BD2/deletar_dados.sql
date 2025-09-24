2b15467d725cb185f9777ab28447f4eaf501706b
yvos77 <yuripb902@gmail.com>
2025-09-23 22:59:40 -0300

    Add files via upload

---
+++ TRABALHO_BD2/deletar_dados.sql
@@ -0,0 +1,9 @@
+-- Deletando os dados das tabelas
+DELETE FROM optd_social.Participacao;
+DELETE FROM optd_social.Oportunidade;
+DELETE FROM optd_social.AreaDeAtuacao;
+DELETE FROM optd_social.Instituicao;
+DELETE FROM optd_social.Voluntario;
+DELETE FROM optd_social.Cadastro;
+DELETE FROM optd_social.Beneficiario;
+
---
+++ TRABALHO_BD2/exibir_resultados.sql
@@ -0,0 +1,8 @@
+-- Selecionando todas as tabelas
+SELECT * FROM optd_social.Beneficiario;
+SELECT * FROM optd_social.Cadastro;
+SELECT * FROM optd_social.Voluntario;
+SELECT * FROM optd_social.Instituicao;
+SELECT * FROM optd_social.AreaDeAtuacao;
+SELECT * FROM optd_social.Oportunidade;
+SELECT * FROM optd_social.Participacao;
